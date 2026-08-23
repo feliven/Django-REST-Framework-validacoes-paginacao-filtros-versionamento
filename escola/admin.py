b3dd472d997bc5e9f3bd422a86de743629504470
FVS-MBA <feliven@gmail.com>
2026-08-22 23:45:11 -0300

    implementa CaseInsensitiveOrderingAdmin

--- escola/admin.py
+++ escola/admin.py
@@ -1,10 +1,41 @@
 from django.contrib import admin
+from django.db import models
 from django.db.models.functions import Lower
 from escola.models import Estudante, Curso, Matricula
 
 
 # Register your models here.
-class ListandoEstudantes(admin.ModelAdmin):
+class CaseInsensitiveOrderingAdmin(admin.ModelAdmin):
+    def get_ordering(self, request):
+        ordering = super().get_ordering(request)
+        if not ordering:
+            return ordering
+
+        result = []
+        for field in ordering:
+            if not isinstance(field, str):
+                result.append(field)
+                continue
+
+            descending = field.startswith("-")
+            clean_field = field.lstrip("-")
+
+            try:
+                model_field = self.model._meta.get_field(clean_field)
+                is_text = isinstance(model_field, (models.CharField, models.TextField))
+            except Exception:
+                is_text = False
+
+            if is_text:
+                expr = Lower(clean_field)
+                result.append(expr.desc() if descending else expr.asc())
+            else:
+                result.append(field)
+
+        return result
+
+
+class ListandoEstudantes(CaseInsensitiveOrderingAdmin):
     list_display = ("id", "nome", "email")
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
@@ -12,23 +43,18 @@ class ListandoEstudantes(admin.ModelAdmin):
     list_editable = ("email",)
     list_per_page = 10
 
-    def get_ordering(self, request):  # type: ignore[override]
-        return [Lower("nome")]
 
-
-class ListandoCursos(admin.ModelAdmin):
+class ListandoCursos(CaseInsensitiveOrderingAdmin):
     list_display = ("codigo", "descricao", "nivel")
     list_display_links = ("codigo",)
     search_fields = ("codigo", "descricao")
     list_filter = ("nivel",)
     list_editable = ("nivel",)
     list_per_page = 10
-
-    def get_ordering(self, request):  # type: ignore[override]
-        return [Lower("codigo"), Lower("descricao")]
+    ordering = ("descricao",)
 
 
-class ListandoMatriculas(admin.ModelAdmin):
+class ListandoMatriculas(CaseInsensitiveOrderingAdmin):
     list_display = ("estudante", "curso", "periodo")
     list_display_links = ("estudante", "curso")
     search_fields = ("codigo",)
